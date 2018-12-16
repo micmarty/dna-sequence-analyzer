3cdfcfff538a6f6ba7449dcd8c4ca85da71352ec
Michał Martyniak <michal.martyniak@linux.pl>
2018-12-16 14:10:29 +0100

    Introduce click support (CLI commands). WIP Refactoring.

--- SequenceAnalyzer.py
+++ SequenceAnalyzer.py
@@ -1,5 +1,5 @@
 import numpy as np
-from typing import Tuple
+from typing import Tuple, Dict, Any
 '''
 Author: Michal Martyniak (github: @micmarty)
 
@@ -33,8 +33,8 @@ class SequencesAnalyzer:
         3: '•'
     }
 
-    def traceback(self, result_matrix, traceback_matrix, start_pos: Tuple[int, int]) -> Tuple[str, str]:
-        # 1.
+    def _traceback(self, result_matrix, traceback_matrix, start_pos: Tuple[int, int]) -> Tuple[str, str]:
+        '''Use both matrices to replay the optimal route'''
         seq_a_aligned = ''
         seq_b_aligned = ''
         row, col = start_pos
@@ -61,54 +61,102 @@ class SequencesAnalyzer:
         # Reverse strings (traceback goes from bottom-right to top-left)
         return seq_a_aligned[::-1], seq_b_aligned[::-1]
 
+    # TODO Refactor score and edit_cost into separate class, allow for loading from file
     def score(self, a, b):
-        #match, mismatch, gap = 3, -2, -1
-        match, mismatch, gap = 2, -2, -1
+        assert isinstance(a, str) and isinstance(b, str)
+        assert len(a) == 1 and len(b) == 1
+
+        match, mismatch, gap = 1, -1, -1
         if a == b:
             return match
-        elif '-' in [a, b]:
-            return gap
         else:
+            if '-' in [a, b]:
+                return gap
             return mismatch
 
-    def editing_cost(self, a, b):
+    def edit_cost(self, a, b):
+        assert isinstance(a, str) and isinstance(b, str)
+        assert len(a) == 1 and len(b) == 1
+
         match, mismatch, gap = 0, 1, 1
         if a == b:
             return match
-        elif '-' in [a, b]:
-            return gap
         else:
+            if '-' in [a, b]:
+                return gap
             return mismatch
 
     def __init__(self, seq_a: str, seq_b: str) -> None:
         self.seq_a = seq_a
         self.seq_b = seq_b
+        # Look for txt files with matrices
 
-    def needleman_wunsch_algorithm(self):
+    def needleman_wunsch_algorithm(self, minimize: bool) -> Dict[str, Any]:
         '''
-        Reference: https://en.wikipedia.org/wiki/Needleman–Wunsch_algorithm
+        Dynamic programming technique
+        Reference: [l3a.pdf, slide #5] + [https://en.wikipedia.org/wiki/Needleman–Wunsch_algorithm]
+        Algorithm: Needleman-Wunch
+        Time complexity: O(nm)
+        Space complexity: O(nm)
         '''
-        pass
+        # Use grid/matrix as graph-like acyclic digraph (array cells are vertices)
+        rows, cols = len(self.seq_a) + 1, len(self.seq_b) + 1
+        H = np.zeros(shape=(rows, cols), dtype=int)
+        traceback = np.zeros(shape=(rows, cols), dtype=np.dtype('U5'))
 
-    def local_alignment(self):
-        result = self.smith_waterman_algorithm()
-        alignment_a, alignment_b = self.traceback(
-            result_matrix=result['result_matrix'],
-            traceback_matrix=result['traceback_matrix'],
-            start_pos=result['score_pos'])
-        print('[Local Alignment] Score={}'.format(result['score']))
-        print(alignment_a)
-        print(alignment_b)
-        
-        
+        # Put sequences' letters into first row and first column (better visualization)
+        traceback[0, 1:] = np.array(list(self.seq_b), dtype=str)
+        traceback[1:, 0] = np.array(list(self.seq_a), dtype=str)
 
-    def smith_waterman_algorithm(self) -> tuple:
+        # 1. Initialize matrix
+        # Top row and leftmost column, like: 0, 1, 2, 3, etc.
+        H[0, :] = np.arange(start=0, stop=cols)
+        H[:, 0] = np.arange(start=0, stop=rows)
+
+        for row in range(1, rows):
+            for col in range(1, cols):
+                # Current pair of letters from sequence A and B
+                a = self.seq_a[row - 1]
+                b = self.seq_b[col - 1]
+
+                if minimize:
+                    # Required for edit cost calculation
+                    score_func = self.edit_cost
+                else:
+                    # Required for similarity calculation
+                    score_func = self.score
+
+                leave_or_replace_letter = H[row-1, col-1] + score_func(a, b)
+                delete_indel = H[row-1, col] + score_func('-', b)
+                insert_indel = H[row, col-1] + score_func(a, '-')
+
+                scores = [leave_or_replace_letter, delete_indel, insert_indel]
+
+                if minimize:
+                    best_action = np.argmin(scores)
+                else:
+                    best_action = np.argmax(scores)
+
+                H[row, col] = scores[best_action]
+                traceback[row, col] = self.traceback_symbols[best_action]
+
+        return {
+            'result_matrix': H,
+            'traceback_matrix': traceback,
+            'score': H[-1, -1],
+            'score_pos': (rows - 1, cols - 1)
+        }
+
+    def smith_waterman_algorithm(self) -> Dict[str, Any]:
         # 1. Prepare dimensions (required additional 1 column and 1 row)
         rows, cols = len(self.seq_a) + 1, len(self.seq_b) + 1
 
         # 2. Initialize matrices
         H = np.zeros(shape=(rows, cols), dtype=int)
         traceback = np.zeros(shape=(rows, cols), dtype=np.dtype('U5'))
+        # Put sequences' letters into first row and first column (better visualization)
+        traceback[0, 1:] = np.array(list(self.seq_b), dtype=str)
+        traceback[1:, 0] = np.array(list(self.seq_a), dtype=str)
 
         # 3. Top row and leftmost colum are already 0
         for row in range(1, rows):
@@ -136,133 +184,32 @@ class SequencesAnalyzer:
             'score_pos': np.unravel_index(np.argmax(H, axis=None), H.shape)
         }
 
-    @property
+    def local_alignment(self) -> Tuple[str, str]:
+        result = self.smith_waterman_algorithm()
+        alignment_a, alignment_b = self._traceback(
+            result_matrix=result['result_matrix'],
+            traceback_matrix=result['traceback_matrix'],
+            start_pos=result['score_pos'])
+
+        print(result['result_matrix'])
+        print(result['traceback_matrix'])
+        print('[Local Alignment] Score={}'.format(result['score']))
+        print(alignment_a)
+        print(alignment_b)
+        return alignment_a, alignment_b
+
     def similarity(self) -> int:
-        '''
-        Dynamic programming technique
-        Reference: l3a.pdf, slide #5 
-        Algorithm: Needleman-Wunch
-        Time complexity: O(nm)
-        Space complexity: O(nm)
-        '''
+        result = self.needleman_wunsch_algorithm(minimize=False)
 
-        # Use grid/matrix as graph-like acyclic digraph (array cells are vertices)
-        rows, cols = len(self.seq_a) + 1, len(self.seq_b) + 1
-        D = np.zeros(shape=(rows, cols), dtype=int)
-        backtrace = np.zeros(shape=(rows, cols), dtype=np.dtype('U5'))
+        print(result['result_matrix'])
+        print(result['traceback_matrix'])
+        print('[Similarity] Score={}'.format(result['score']))
+        return result['score']
 
-        # 1. Initialize matrix
-        # Top row and leftmost column, like: 0, 1, 2, 3, etc.
-        D[0, :] = np.arange(start=0, stop=cols)
-        D[:, 0] = np.arange(start=0, stop=rows)
-
-        for row in range(1, rows):
-            for col in range(1, cols):
-                # Current pair of letters from sequence A and B
-                a = self.seq_a[row - 1]
-                b = self.seq_b[col - 1]
-
-                # Determine what's the editing cost
-                cost = self.replacement_cost[a][b]
-                print(f'Current pair: "{a}" -> "{b}", cost={cost}')
-
-                letter_replacement = D[row-1, col-1] + 1  # cost(a[i], b[i])
-                delete_indel = D[row-1, col] + 1  # cost    # cost('-', b[i])
-                insert_indel = D[row, col-1] + 1  # cost # cost(a[i], '-')
-
-                operations_costs = [letter_replacement,
-                                    delete_indel, insert_indel]
-                min_operation = np.argmax(operations_costs)
-
-                D[row, col] = operations_costs[min_operation]
-
-                if min_operation == 0:
-                    op = f'↖ ({cost})'
-                elif min_operation == 1:
-                    op = f'↑ ({cost})'
-                elif min_operation == 2:
-                    op = f'← ({cost})'
-                backtrace[row, col] = op
-                # print(
-                #     f'[0] Letter replacement={letter_replacement} | ' \
-                #     f'[1] Replace indel={delete_indel} | ' \
-                #     f'[2] Insert indel={insert_indel} | ' \
-                #     f'Operation: [{min_operation}]')
-        # Bottom-right cell contains optimal distance
-        min_distance = D[-1, -1]
-
-        print('[Similarity matrix]\n', D)
-        print('[Backtrace matrix] Follow the arrows (cost in brackets)\n', backtrace)
-        print('Similarity =', min_distance)
-        return min_distance
-
-    @property
     def edit_distance(self) -> int:
-        '''
-        Dynamic programming technique
-        Reference: l3a.pdf, slide #5 
-        Algorithm: Needleman-Wunch
-        Time complexity: O(nm)
-        Space complexity: O(nm)
-        '''
+        result = self.needleman_wunsch_algorithm(minimize=True)
 
-        # Use grid/matrix as graph-like acyclic digraph (array cells are vertices)
-        rows, cols = len(self.seq_a) + 1, len(self.seq_b) + 1
-        D = np.zeros(shape=(rows, cols), dtype=int)
-        backtrace = np.zeros(shape=(rows, cols), dtype=np.dtype('U5'))
-
-        # 1. Initialize matrix
-        # Top row and leftmost column, like: 0, 1, 2, 3, etc.
-        D[0, :] = np.arange(start=0, stop=cols)
-        D[:, 0] = np.arange(start=0, stop=rows)
-
-        for row in range(1, rows):
-            for col in range(1, cols):
-                # Current pair of letters from sequence A and B
-                a = self.seq_a[row - 1]
-                b = self.seq_b[col - 1]
-
-                # Determine what's the editing cost
-                #cost = self.replacement_cost[a][b]
-                cost = self.editing_cost(a, b)
-                print(f'Current pair: "{a}" -> "{b}", cost={cost}')
-
-                letter_replacement = D[row-1, col-1] + cost  # cost(a[i], b[i])
-                delete_indel = D[row-1, col] + cost    # cost('-', b[i])
-                insert_indel = D[row, col-1] + cost  # cost(a[i], '-')
-
-                operations_costs = [letter_replacement,
-                                    delete_indel, insert_indel]
-                min_operation = np.argmin(operations_costs)
-
-                D[row, col] = operations_costs[min_operation]
-
-                if min_operation == 0:
-                    op = f'↖ ({cost})'
-                elif min_operation == 1:
-                    op = f'↑ ({cost})'
-                elif min_operation == 2:
-                    op = f'← ({cost})'
-                backtrace[row, col] = op
-                # print(
-                #     f'[0] Letter replacement={letter_replacement} | ' \
-                #     f'[1] Replace indel={delete_indel} | ' \
-                #     f'[2] Insert indel={insert_indel} | ' \
-                #     f'Operation: [{min_operation}]')
-        # Bottom-right cell contains optimal distance
-        min_distance = D[-1, -1]
-
-        print('[Distance matrix]\n', D)
-        print('[Backtrace matrix] Follow the arrows (cost in brackets)\n', backtrace)
-        print('Min edit distance =', min_distance)
-        return min_distance
-
-
-#SequencesAnalyzer.edit_distance('AGTTT', 'AT-')
-#SequencesAnalyzer('AGCACACA', 'ACACACTA').similarity
-
-
-
-
-#SequencesAnalyzer('AACTTAC', 'TGAATTT').local_alignment()
-SequencesAnalyzer('xyaxbacsll', 'pqraxabcstvq' ).local_alignment()
+        print(result['result_matrix'])
+        print(result['traceback_matrix'])
+        print('[Edit distance] Cost={}'.format(result['score']))
+        return result['score']
---
+++ analyze.py
@@ -0,0 +1,31 @@
+import click
+from SequenceAnalyzer import SequencesAnalyzer
+
+@click.command()
+@click.argument('sequence_a')
+@click.argument('sequence_b')
+@click.option('-S', '--summary', is_flag=True)
+@click.option('-s', '--similarity', is_flag=True)
+@click.option('-e', '--edit-distance', is_flag=True)
+@click.option('-a', '--alignment', type=click.Choice(['global', 'local']))
+def main(summary, similarity, edit_distance, sequence_a, sequence_b, alignment):
+    analyzer = SequencesAnalyzer(sequence_a, sequence_b)
+
+    if summary:
+        # analyzer.edit_distance()
+        # analyzer.similarity()
+        analyzer.local_alignment()
+        # analyzer.global_alignment()
+    if similarity:
+        analyzer.similarity()
+    if edit_distance:
+        analyzer.edit_distance()
+    if alignment == 'local':
+        analyzer.local_alignment()
+    elif alignment == 'global':
+        #analyzer.global_alignment()
+        pass
+    # TODO edit + similarity
+
+if __name__ == '__main__':
+    main()
